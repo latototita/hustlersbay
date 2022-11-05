ad566daa3a70de954390a7b1a2cbeb9a84976538
latototita <85679433+latototita@users.noreply.github.com>
2022-11-05 12:01:37 +0300

    Add files via upload

--- scumtrader/settings.py
+++ scumtrader/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    
 ]
 
 MIDDLEWARE = [
@@ -141,7 +140,3 @@ EMAIL_USE_TLS=True
 EMAIL_HOST_USER='pearlmartbusinesses@yahoo.com'
 EMAIL_HOST_PASSWORD='jlxmlemwkciomuea'
 
-CRON_CLASSES = [
-    "main.cron.MyCronJob",
-    # ...
-]
