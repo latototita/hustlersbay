cf7dfa47d6daf2b96c638dd70bc041118f6a9157
latototita <85679433+latototita@users.noreply.github.com>
2022-11-09 10:19:12 +0300

    Update settings.py

--- scumtrader/settings.py
+++ scumtrader/settings.py
@@ -103,7 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
